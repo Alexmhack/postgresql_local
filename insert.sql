2734673a2b78b784e589210867edc108ec3e9319
alexHooper <alexmohack@gmail.com>
2018-08-03 14:08:43 +0530

    create file for inserting data

---
+++ insert.sql
@@ -0,0 +1,7 @@
+INSERT INTO flights (origin, destination, duration) VALUES ('Indore', 'Delhi', 120);
+INSERT INTO flights (origin, destination, duration) VALUES ('New York', 'Italy', 540);
+INSERT INTO flights (origin, destination, duration) VALUES ('Indore', 'Kerela', 480);
+INSERT INTO flights (origin, destination, duration) VALUES ('Delhi', 'California', 1440);
+INSERT INTO flights (origin, destination, duration) VALUES ('Palo Alto', 'Sydney', 600);
+INSERT INTO flights (origin, destination, duration) VALUES ('Mumbai', 'Delhi', 180);
+INSERT INTO flights (origin, destination, duration) VALUES ('Goa', 'Canberra', 300);
