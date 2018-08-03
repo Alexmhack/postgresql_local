66949152895607c001f78cdb114d238ac6d0922d
alexHooper <alexmohack@gmail.com>
2018-08-03 19:14:14 +0530

    insert values into passenger table

--- insert.sql
+++ insert.sql
@@ -5,3 +5,18 @@ INSERT INTO flights (origin, destination, duration) VALUES ('Delhi', 'California
 INSERT INTO flights (origin, destination, duration) VALUES ('Palo Alto', 'Sydney', 600);
 INSERT INTO flights (origin, destination, duration) VALUES ('Mumbai', 'Delhi', 180);
 INSERT INTO flights (origin, destination, duration) VALUES ('Goa', 'Canberra', 300);
+
+
+INSERT INTO passengers (name, flight_id) VALUES ('Ethan', 1);
+INSERT INTO passengers (name, flight_id) VALUES ('Kunal', 2);
+INSERT INTO passengers (name, flight_id) VALUES ('Adi', 3);
+INSERT INTO passengers (name, flight_id) VALUES ('Pranav', 4);
+INSERT INTO passengers (name, flight_id) VALUES ('PSG', 5);
+INSERT INTO passengers (name, flight_id) VALUES ('Alex', 6);
+INSERT INTO passengers (name, flight_id) VALUES ('Jackie', 1);
+INSERT INTO passengers (name, flight_id) VALUES ('Bruno', 2);
+INSERT INTO passengers (name, flight_id) VALUES ('Aman', 3);
+INSERT INTO passengers (name, flight_id) VALUES ('Divyam', 4);
+INSERT INTO passengers (name, flight_id) VALUES ('Rishabh', 4);
+INSERT INTO passengers (name, flight_id) VALUES ('Pranjal', 5);
+INSERT INTO passengers (name, flight_id) VALUES ('Archit', 6);
