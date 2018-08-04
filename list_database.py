f61d526a9a48217f926577d56e28c6190d7f905b
alexHooper <alexmohack@gmail.com>
2018-08-04 12:33:06 +0530

    put the code in a function

--- list_database.py
+++ list_database.py
@@ -4,7 +4,11 @@ from sqlalchemy.orm import scoped_session, sessionmaker
 engine = create_engine('postgresql://postgres:admin@localhost/postgres')
 db = scoped_session(sessionmaker(bind=engine))
 
-result = engine.execute("SELECT * FROM flights")
+def main():
+	results = db.execute("SELECT * FROM flights").fetchall()
+	for res in results:
+		print(res)
 
-for res in result:
-	print(f"{res.origin} to {res.destination}, {res.duration}")
+
+if __name__ == '__main__':
+	main()
