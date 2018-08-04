026bcc16aff0f9cb10b283c1567bf6e56dbc2a47
alexHooper <alexmohack@gmail.com>
2018-08-04 11:30:49 +0530

    change the column names with asterisk

--- list_database.py
+++ list_database.py
@@ -4,7 +4,7 @@ from sqlalchemy.orm import scoped_session, sessionmaker
 engine = create_engine('postgresql://postgres:admin@localhost/postgres')
 db = scoped_session(sessionmaker(bind=engine))
 
-result = engine.execute("SELECT origin, destination, duration FROM flights")
+result = engine.execute("SELECT * FROM flights")
 
 for res in result:
 	print(f"{res.origin} to {res.destination}, {res.duration}")
