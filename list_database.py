81dab76e4349236103c25662c90f1f3f3a73b5ee
alexHooper <alexmohack@gmail.com>
2018-08-04 09:52:52 +0530

    create sql engine and run query

--- list_database.py
+++ list_database.py
@@ -1,4 +1,15 @@
 import os
 
 from sqlalchemy import create_engine
-from sqlalchemy.orm import scoped_session, sessionmake
+from sqlalchemy.orm import scoped_session, sessionmaker
+
+engine = create_engine(os.getenv("DATABASE_URL"))
+db = scoped_session(sessionmaker(bind=engine))
+
+def main():
+	flights = db.execute("SELECT * FROM flights").fetchall()
+	print(flights)
+
+
+if __name__ == '__main__':
+	main()
