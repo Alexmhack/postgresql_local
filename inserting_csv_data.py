ea96a54472f78cf054e98a3f117886b5e3346df2
alexHooper <alexmohack@gmail.com>
2018-08-04 12:50:38 +0530

    inerting csv data from csv file using csv module

---
+++ inserting_csv_data.py
@@ -0,0 +1,22 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, scoped_session
+
+import csv
+
+engine = create_engine("postgresql://postgres:admin@localhost/postgres")
+db = scoped_session(sessionmaker(bind=engine))
+
+def main():
+	csv_data = open('flights_data.csv')
+	reader = csv.reader(csv_data)
+
+	for origin, destination, duration in reader:
+		db.execute("INSERT INTO flights (origin, destination, duration) VALUES (:origin, :destination, :duration)",
+			{'origin': origin, 'destination': destination, 'duration': duration}
+		)
+		db.commit()
+		print(f"INSERTED:\n\t ORIGIN:{origin} DESTINATION: {destination} DURATION: {duration}")
+
+
+if __name__ == "__main__":
+	main()
